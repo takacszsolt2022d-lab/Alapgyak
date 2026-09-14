4e3c35ae038e0172320a346aea5d274dca5f0783
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-14 11:49:47 +0200

    negyedikfeladat

---
+++ Raktarkezelo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Raktarkezelo;
+
+
+namespace Raktarkezelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RaktarElem termek = new RaktarElem();
+            termek.CikkNev = "USB-C Kábel (2m)";
+
+            Console.WriteLine($"=== Raktárelem kezelése: {termek.CikkNev} ===");
+            Console.WriteLine($"Kezdő készlet: {termek.DbSzam} db\n");
+
+            Console.WriteLine("--- Bevételezés ---");
+            termek.Bevetelezes(-5); 
+            termek.Bevetelezes(50); 
+            Console.WriteLine();
+
+
+            Console.WriteLine("--- Kiadás ---");
+            termek.Kiadas(20); 
+            termek.Kiadas(40);  
+            termek.Kiadas(-10);
+            Console.WriteLine();
+
+            Console.WriteLine("--- Árszámítás (Statikus metódus) ---");
+            double nettoAr = 3500;
+            double bruttoAr = RaktarElem.BruttoArSzamitas(nettoAr);
+
+            Console.WriteLine($"ÁFA kulcs: {RaktarElem.AfaKulcs * 100}%");
+            Console.WriteLine($"Nettó ár: {nettoAr} Ft");
+            Console.WriteLine($"Bruttó ár: {bruttoAr} Ft");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Raktarkezelo/RaktarElem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raktarkezelo
+{
+    public class RaktarElem
+    {
+        private int dbSzam = 0;
+
+        public string CikkNev { get; set; }
+
+        public static double AfaKulcs = 0.27;
+
+        public int DbSzam
+        {
+            get { return dbSzam; }
+        }
+
+        public void Bevetelezes(int db)
+        {
+            if (db <= 0)
+            {
+                Console.WriteLine("Figyelmeztetés: A bevételezett mennyiségnek pozitívnak kell lennie!");
+            }
+            else
+            {
+                dbSzam += db;
+                Console.WriteLine($"Sikeres bevételezés: +{db} db. Jelenlegi készlet: {dbSzam} db");
+            }
+        }
+
+        public void Kiadas(int db)
+        {
+            if (db <= 0)
+            {
+                Console.WriteLine("Figyelmeztetés: A kiadni kívánt mennyiségnek pozitívnak kell lennie!");
+            }
+            else if (dbSzam - db < 0)
+            {
+                Console.WriteLine($"Figyelmeztetés: Nincs elegendő készlet! (Kért: {db} db, Elérhető: {dbSzam} db)");
+            }
+            else
+            {
+                dbSzam -= db;
+                Console.WriteLine($"Sikeres kiadás: -{db} db. Jelenlegi készlet: {dbSzam} db");
+            }
+        }
+        public static double BruttoArSzamitas(double nettoAr)
+        {
+            return nettoAr * (1 + AfaKulcs);
+        }
+    }
+}
