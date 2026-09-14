77b4917eda563dff19595b6c769067b4c622c7d4
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-14 10:41:28 +0200

    elsofeladat

---
+++ WebaruhazTermek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using WebaruhazTermek;
+
+namespace WebaruhazTermek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Termek termek1 = new Termek();
+            termek1.Nev = "Vezeték nélküli egér";
+            termek1.Egysegar = 4500;
+            termek1.Keszlet = 12;
+
+            Termek termek2 = new Termek();
+            termek2.Nev = "Promóciós kulcstartó";
+            termek2.Egysegar = -500; 
+            termek2.Keszlet = -5;    
+
+            Console.WriteLine("--- 1. Termék (Érvényes adatok) ---");
+            Console.WriteLine($"Név: {termek1.Nev}");
+            Console.WriteLine($"Egységár: {termek1.Egysegar} Ft");
+            Console.WriteLine($"Készlet: {termek1.Keszlet} db\n");
+
+            Console.WriteLine("--- 2. Termék (Érvénytelen adatok korrigálva) ---");
+            Console.WriteLine($"Név: {termek2.Nev}");
+            Console.WriteLine($"Egységár: {termek2.Egysegar} Ft");
+            Console.WriteLine($"Készlet: {termek2.Keszlet} db");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebaruhazTermek/Termek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebaruhazTermek
+{
+    public class Termek
+    {
+        private int egysegar;
+        private int keszlet;
+
+        public string Nev { get; set; }
+
+
+        public int Egysegar
+        {
+            get { return egysegar; }
+            set
+            {
+                if (value < 1)
+                {
+                    egysegar = 1;
+                }
+                else
+                {
+                    egysegar = value;
+                }
+            }
+        }
+
+        public int Keszlet
+        {
+            get { return keszlet; }
+            set
+            {
+                if (value < 0)
+                {
+                    keszlet = 0;
+                }
+                else
+                {
+                    keszlet = value;
+                }
+            }
+        }
+    }
+}
