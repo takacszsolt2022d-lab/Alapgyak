566b88b36ea034622406ed7eff222767f041cd64
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-14 11:03:58 +0200

    harmadikfeladat

---
+++ FelhasznaloFiok/Felhasznalo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FelhasznaloFiok
+{
+    public class Felhasznalo
+    {
+        private string jelszo = "123456";
+
+        public string Felhasznalonev { get; set; }
+
+        public void JelszoModositas(string regiJelszo, string ujJelszo)
+        {
+            if (regiJelszo != jelszo)
+            {
+                Console.WriteLine("Hiba: A megadott régi jelszó helytelen!");
+            }
+            else if (ujJelszo.Length < 6)
+            {
+                Console.WriteLine("Hiba: Az új jelszónak legalább 6 karakter hosszúnak kell lennie!");
+            }
+            else
+            {
+                jelszo = ujJelszo;
+                Console.WriteLine("Siker: A jelszó sikeresen módosítva lett!");
+            }
+        }
+    }
+}
---
+++ FelhasznaloFiok/Program.cs
@@ -0,0 +1,35 @@
+﻿using FelhasznaloFiok;
+
+namespace FelhasznaloFiok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Felhasznalo user = new Felhasznalo();
+            user.Felhasznalonev = "kovacs_pete";
+
+            Console.WriteLine($"Felhasználó létrehozva: {user.Felhasznalonev}\n");
+
+
+            Console.WriteLine("--- Jelszómódosítás tesztelése ---\n");
+
+            Console.WriteLine("1. Próbálkozás (hibás régi jelszó):");
+            user.JelszoModositas("rosszJelszo", "ujJelszo123");
+            Console.WriteLine();
+
+            Console.WriteLine("2. Próbálkozás (túl rövid új jelszó):");
+            user.JelszoModositas("123456", "123");
+            Console.WriteLine();
+
+            Console.WriteLine("3. Próbálkozás (helyes adatok):");
+            user.JelszoModositas("123456", "ujBiztonsagosJelszo123");
+            Console.WriteLine();
+
+            Console.WriteLine("4. Módosítás ellenőrzése az új jelszóval:");
+            user.JelszoModositas("ujBiztonsagosJelszo123", "masikSikeresJelszo");
+
+            Console.ReadLine();
+        }
+    }
+}
