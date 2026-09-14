b7686e60fc687b693aa153032a883f1b01d4fd7e
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-14 10:46:33 +0200

    masodikfeladat

---
+++ Mertekegyseg/Atvalto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mertekegyseg
+{
+    public class Atvalto
+    {
+        public double Szorzo;
+
+        public static double KmBolMeter(double km)
+        {
+            return km * 1000;
+        }
+
+        public double EgyediAtvaltas(double ertek)
+        {
+            return ertek * Szorzo;
+        }
+    }
+}
---
+++ Mertekegyseg/Program.cs
@@ -0,0 +1,28 @@
+﻿using Mertekegyseg;
+
+namespace Mertekegyseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km = 5.5;
+            double meter = Atvalto.KmBolMeter(km);
+            Console.WriteLine($"--- Statikus metódus tesztelése ---");
+            Console.WriteLine($"{km} km = {meter} méter\n");
+
+            Atvalto merfoldAtvalto = new Atvalto();
+
+            merfoldAtvalto.Szorzo = 1.60934;
+
+            double merfold = 10;
+            double atvaltomerfold = merfoldAtvalto.EgyediAtvaltas(merfold);
+
+            Console.WriteLine($"--- Példányosított metódus tesztelése ---");
+            Console.WriteLine($"Beállított szorzó: {merfoldAtvalto.Szorzo}");
+            Console.WriteLine($"{merfold} mérföld = {atvaltomerfold} km");
+
+            Console.ReadLine();
+        }
+    }
+}
